9bc67bbf737becb6b443e415afb4d52e1a229adc
blieber <blieber@umich.edu>
2023-04-19 16:49:01 -0400

    fixing creating folders

--- splice_video.py
+++ splice_video.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--video', default='./driving.mp4')
@@ -11,6 +12,8 @@ video_loc = opt.video
 output_dir = opt.output_dir
 
 video = cv2.VideoCapture(video_loc)
+
+Path(output_dir).mkdir(exist_ok=True)
 
 success,image = video.read()
 count = 0
