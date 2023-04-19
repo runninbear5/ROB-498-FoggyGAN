866b6c6a794d61570fad97248db5d35318b0432e
blieber <blieber@umich.edu>
2023-04-19 16:59:46 -0400

    fixing more issues

--- combine_video.py
+++ combine_video.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_dir', default='./video_output')
+parser.add_argument('--input_dir', default='./video_results')
 parser.add_argument('--output_video', default='./results.avi')
 
 opt = parser.parse_args()
